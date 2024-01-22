38cdf88a9577d5cb9effb894310cb446f719955b
kpodsiadlo7 <karolekk131@gmail.com>
2024-01-22 14:48:38 +0100

    Initial commit

---
+++ src/main/java/com/lol/stats/LeagueOfLegendsStatsApplication.java
@@ -0,0 +1,13 @@
+package com.lol.stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueOfLegendsStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeagueOfLegendsStatsApplication.class, args);
+    }
+
+}
